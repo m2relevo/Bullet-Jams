7240f0bf4cb748e3b62928d768b3a43d035e9fc6
mp2-cole <colemarcus@hotmail.co.uk>
2016-01-19 21:24:46 +0000

    Super fast Stream

--- kJam/Spac In'vadurrs/Assets/Scripts/ExplosionPattern.cs
+++ kJam/Spac In'vadurrs/Assets/Scripts/ExplosionPattern.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ExplosionPattern : MonoBehaviour {
 
-	public float speed = 5f;
+	public float speed = 2f;
 	GameObject missile;
 	GameObject BossObj;
     GameObject[] Cannonballs;
@@ -13,36 +13,43 @@ public class ExplosionPattern : MonoBehaviour {
 		Cannonballs = new GameObject[10];
 		missile = Resources.Load ("BossBullet") as GameObject;
 		BossObj = GameObject.FindGameObjectWithTag ("enemy");
-		Cannonballs [0] = (GameObject)Instantiate (missile, new Vector3 (-3.92F, -2.91F, 0F), Quaternion.identity);
-		Cannonballs [1] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [2] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [3] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [4] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [5] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [6] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [7] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [8] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
-		Cannonballs [9] = (GameObject)Instantiate (missile, new Vector3 (-1.27F, -1.14F, 0F), Quaternion.identity);
+		Cannonballs [0] = (GameObject)Instantiate (missile, new Vector3 (-2.27F, -1.83F, 0F), Quaternion.identity);
+		Cannonballs [1] = (GameObject)Instantiate (missile, new Vector3 (-1.95F, -2.09F, 0F), Quaternion.identity);
+		Cannonballs [2] = (GameObject)Instantiate (missile, new Vector3 (-1.5F, -2.16F, 0F), Quaternion.identity);
+		Cannonballs [3] = (GameObject)Instantiate (missile, new Vector3 (-1.0F, -2.09F, 0F), Quaternion.identity);
+		Cannonballs [4] = (GameObject)Instantiate (missile, new Vector3 (-0.62F, -1.83F, 0F), Quaternion.identity);
+		Cannonballs [5] = (GameObject)Instantiate (missile, new Vector3 (-2.27F, -1.33F, 0F), Quaternion.identity);
+		Cannonballs [6] = (GameObject)Instantiate (missile, new Vector3 (-1.95F, -0.97F, 0F), Quaternion.identity);
+		Cannonballs [7] = (GameObject)Instantiate (missile, new Vector3 (-1.5F, -0.86F, 0F), Quaternion.identity);
+		Cannonballs [8] = (GameObject)Instantiate (missile, new Vector3 (-1.0F, -0.97F, 0F), Quaternion.identity);
+		Cannonballs [9] = (GameObject)Instantiate (missile, new Vector3 (-0.62F, -1.33F, 0F), Quaternion.identity);
 
 	}
 	// Update is called once per frame
 	public void Update () 
 	{
-		float step = speed * Time.deltaTime;
-
-		    if (Cannonballs[0] != null) 
-		    {
-			Cannonballs [0].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (-3.920F, -2.91F, 0F), step);
-			Cannonballs [1].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (-2.010F, -2.91F, 0F), step);
-			Cannonballs [2].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (-0.100F, -2.91F, 0F), step);
-			Cannonballs [3].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (1.810F, -2.91F, 0F), step);
-			Cannonballs [4].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (3.720F, -2.91F, 0F), step);
-			Cannonballs [5].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (-3.920F, 2.23F, 0F), step);
-			Cannonballs [6].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (-2.010F, 2.23F, 0F), step);
-			Cannonballs [7].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (-0.100F, -2.91F, 0F), step);
-			Cannonballs [8].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (1.810F, -2.91F, 0F), step);
-			Cannonballs [9].transform.position = Vector3.MoveTowards (transform.position, new Vector3 (3.720F, -2.91F, 0F), step);
-		   }
+		float step = speed * Time.deltaTime; 
+		if(Cannonballs[0] != null)
+		Cannonballs [0].transform.position = Vector3.MoveTowards (Cannonballs[0].transform.position, new Vector3 (-3.920F, -5.0F, 0F), step);
+		if(Cannonballs[1] != null)
+			Cannonballs [1].transform.position = Vector3.MoveTowards (Cannonballs[1].transform.position, new Vector3 (-2.010F, -5.0F, 0F), step);
+		if(Cannonballs[2] != null)
+			Cannonballs [2].transform.position = Vector3.MoveTowards (Cannonballs[2].transform.position, new Vector3 (-0.100F, -5.0F, 0F), step);
+		if(Cannonballs[3] != null)
+			Cannonballs [3].transform.position = Vector3.MoveTowards (Cannonballs[3].transform.position, new Vector3 (1.810F, -5.0F, 0F), step);
+		if(Cannonballs[4] != null)
+			Cannonballs [4].transform.position = Vector3.MoveTowards (Cannonballs[4].transform.position, new Vector3 (3.720F, -5.0F, 0F), step);
+		if(Cannonballs[5] != null)
+			Cannonballs [5].transform.position = Vector3.MoveTowards (Cannonballs[5].transform.position, new Vector3 (-3.920F, 3.0F, 0F), step);
+		if(Cannonballs[6] != null)
+			Cannonballs [6].transform.position = Vector3.MoveTowards (Cannonballs[6].transform.position, new Vector3 (-2.010F, 3.0F, 0F), step);
+		if(Cannonballs[7] != null)
+			Cannonballs [7].transform.position = Vector3.MoveTowards (Cannonballs[7].transform.position, new Vector3 (-0.100F, 3.0F, 0F), step);
+		if(Cannonballs[8] != null)
+			Cannonballs [8].transform.position = Vector3.MoveTowards (Cannonballs[8].transform.position, new Vector3 (1.810F, 3.0F, 0F), step);
+		if(Cannonballs[9] != null)
+			Cannonballs [9].transform.position = Vector3.MoveTowards (Cannonballs[9].transform.position, new Vector3 (3.720F, 3.0F, 0F), step);
+		   
 		int j = 0;
 		for (int i = 0; i<10; i++) 
 		{
@@ -51,11 +58,12 @@ public class ExplosionPattern : MonoBehaviour {
 				j++;
 			}
 		}
-		if(j == 9)
+		if(j == 10)
 		{
 			endScript();
 		}
 	}
+
 	void endScript()
 	{
 		this.gameObject.GetComponent<Boss> ().scriptFinished ();
